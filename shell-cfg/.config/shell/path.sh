f874dd593293780e49fc57df3dd14e93826327da
msdlr <miguelsrosa97@gmail.com>
2022-02-27 22:36:32 +0100

    shell: Removed ldconfig command on PATH-setting script

--- shell-cfg/.config/shell/path.sh
+++ shell-cfg/.config/shell/path.sh
@@ -3,7 +3,6 @@
 # PATH + LD for .local/{bin,lib,etc...} for local, non-root user
 [ -d ${HOME}/.local/bin ] && [[ ":${PATH}:" == *"${HOME}/.local/bin"* ]] || export PATH=${HOME}/.local/bin:${PATH}
 [ -d ${HOME}/.local/lib ] && [[ ":${LD_LIBRARY_PATH}:" == *"${HOME}/.local/lib"* ]] || export LD_LIBRARY_PATH=${HOME}/.local/lib:${LD_LIBRARY_PATH}
-[ -d ${HOME}/.local/lib ] && ldconfig -n ${HOME}/.local/lib
 
 # PATH for scripts directory
 [ -d ${HOME}/.local/scripts ] && [[ ":${PATH}:" == *"${HOME}/.local/scripts"* ]] || export PATH=${HOME}/.local/scripts:${PATH}
