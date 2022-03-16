c28426f2904313073d17c52d8a75d4634f3514f6
msdlr <miguelsrosa97@gmail.com>
2022-03-16 09:09:29 +0100

    shell: $EDITOR and $VISUAL envvars

--- shell-cfg/.config/shell/env.sh
+++ shell-cfg/.config/shell/env.sh
@@ -3,3 +3,6 @@
 export XDG_CONFIG_HOME=${HOME}/.config
 export XDG_CACHE_HOME=${HOME}/.cache
 export XDG_DATA_HOME=${HOME}/.local/share
+
+sh -c "which vim" >/dev/null && export EDITOR="vim" || export EDITOR="nano"
+sh -c "which vim" >/dev/null && export VISUAL="vim" || export VISUAL="nano"
