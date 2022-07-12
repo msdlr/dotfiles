c6e2690f70b764cac409c676203512b8010f2985
msdlr <miguelsrosa97@gmail.com>
2022-07-12 09:46:03 +0200

    Added config for GNU screen with scroll support

--- shell-cfg/.config/shell/env.sh
+++ shell-cfg/.config/shell/env.sh
@@ -6,3 +6,6 @@ export XDG_DATA_HOME=${HOME}/.local/share
 
 sh -c "which vim" >/dev/null && export EDITOR="vim" || export EDITOR="nano"
 sh -c "which vim" >/dev/null && export VISUAL="vim" || export VISUAL="nano"
+
+# XDG partially-supported configs
+export SCREENRC="${XDG_CONFIG_HOME}"/screen/screenrc
