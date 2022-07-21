bbffcc56a285eb372e2823cfa70944b3ee2e0d71
msdlr <miguelsrosa97@gmail.com>
2022-07-21 22:30:54 +0200

    env: $DISPLAY value for WSL hosts

--- shell-cfg/.config/shell/env.sh
+++ shell-cfg/.config/shell/env.sh
@@ -9,3 +9,10 @@ sh -c "which vim" >/dev/null && export VISUAL="vim" || export VISUAL="nano"
 
 # XDG partially-supported configs
 export SCREENRC="${XDG_CONFIG_HOME}"/screen/screenrc
+
+# $DISPLAY for WSL
+
+if [ $(expr "$(uname --kernel-release)" : ".*WSL.*") != "0"  ]
+then
+	DISPLAY=${DISPLAY:="$(grep -m 1 nameserver /etc/resolv.conf | awk '{print $2}'):0.0"}
+fi
