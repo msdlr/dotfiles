a96310a784158f989aa9eee90f3dbe1d57f40ba6
msdlr <miguelsrosa97@gmail.com>
2022-10-29 15:59:02 +0200

    Reworked (remote) copy script

---
+++ copy.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+export R=${R:=$(hostname)}
+export U=${U:=$(ssh ${R} whoami)}
+
+[ "${R}" = "$(hostname)" ] || ssh-copy-id ${U}@${R} 2>/dev/null
+
+[ $# -eq 0 ] && ${0} $(find . -maxdepth 1 -type d -not -name '.' -not -name '.git')
+
+for pkg in $@
+do
+    if [ "${R}" = "$(hostname)" ] 
+    then
+        [ -d ${pkg} ] && cp --remove-destination -rv $(find ${pkg} -maxdepth 1 -not -name ${pkg}) ~
+    else    
+        [ -d ${pkg} ] && scp -r $(find ${pkg} -maxdepth 1 -mindepth 1 -not -name ${pkg}) ${U}@${R}:
+    fi
+done
