eee99c6f898de8b2f8269b1f3ccc0c592bfe4196
msdlr <miguelsrosa97@gmail.com>
2023-08-23 15:03:44 +0200

    Simplified target directory structure creation in link script

--- link.sh
+++ link.sh
@@ -6,25 +6,7 @@ stowlike () {
     PACKAGE=${1}
     
     cd ${PACKAGE}
-    
-    # Create directory structure
-    for dir in $(find . -type d -mindepth 1 2>/dev/null)
-    do
-        TARGETDIR=$(echo $dir | sed "s|^.|${TARGET}|g")
-        #echo "CREATE DIR ${TARGETDIR}" #| sed -e "s/$PACKAGE//d"
-        mkdir -pv ${TARGETDIR} 2>/dev/null
-    done
-    
-    # Link regular files
-    for file in $(find . -type f -mindepth 1 2>/dev/null)
-    do
-        SOURCEFILE=$(realpath ${file})
-        TARGETFILE=$(echo ${file} | sed "s|^.|${TARGET}|g")
-        
-        # echo "Link ${SOURCEFILE} @ ${TARGETFILE}"
-        rm ${TARGETFILE} 2>/dev/null # Remove target file to override it
-        ln -sv ${SOURCEFILE} ${TARGETFILE} 2>/dev/null
-    done
+    cp -rsv $(pwd)/ ${TARGET}
     
     cd $OLDPWD
 }
@@ -35,12 +17,12 @@ case $# in
         #find . -type d -maxdepth 1 2>/dev/null | sed '/^.*/d ; s|./||g'
         for p in $(find . -type d -maxdepth 1 -mindepth 1 -not -name '.*' 2>/dev/null)
         do
-            #echo "Installing ${p}"
+            echo "Installing ${p} config"
             stowlike ${p}
         done
         ;;
     "1")
-        #echo "Installing ${1}"
+        echo "Installing ${1} config"
         [ -d ${1} ] && stowlike ${1}
         ;;
     *)
