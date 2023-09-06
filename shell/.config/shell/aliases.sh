57f773997554d8ad36763f387363cb0f821ca94f
msdlr <miguelsrosa97@gmail.com>
2023-09-06 08:35:59 +0200

    alias: gpull no longer sets upstream remote branch

--- shell/.config/shell/aliases.sh
+++ shell/.config/shell/aliases.sh
@@ -42,7 +42,7 @@ then
   alias gsu='git branch --set-upstream-to=origin/"$(git symbolic-ref --short HEAD)" "$(git symbolic-ref --short HEAD)"'
   alias gpush='git push'
   alias gpushu='git push --set-upstream $(git remote show) $(git rev-parse --abbrev-ref HEAD)'
-  alias gpull='git submodule init; git pull --recurse-submodules --set-upstream $(git remote show) $(git rev-parse --abbrev-ref HEAD)'
+  alias gpull='git submodule init; git pull --recurse-submodules'
   alias grh='git reset --hard'
   alias grs='git reset --soft'
   alias grm='git reset --mixed'
