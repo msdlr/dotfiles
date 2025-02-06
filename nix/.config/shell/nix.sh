6c4b91864da7c92f287023ac7acce5b797eaa008
msdlr <miguelsrosa97@gmail.com>
2025-02-06 15:41:59 +0100

    nix: fix nix profile check

--- nix/.config/shell/nix.sh
+++ nix/.config/shell/nix.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 # Detect local user installation
-if [ -e ${HOME}/.nix-profile/etc/profile.d/nix.sh ]
+if [ -f ${HOME}/.nix-profile/etc/profile.d/nix.sh ]
 then
 	source ${HOME}/.nix-profile/etc/profile.d/nix.sh
 fi
