11b553790474f9c1e6cab1ce4995a17f890f1552
msdlr <miguelsrosa97@gmail.com>
2025-02-10 16:37:49 +0100

    nix: functions to install all packages or select interactively

--- nix/.config/shell/nix.sh
+++ nix/.config/shell/nix.sh
@@ -1,7 +1,31 @@
 #!/usr/bin/env sh
 
+nix_setup_interactive() {
+    CONFIG_FILE="$HOME/.config/nixpkgs/config.nix"
+    categories="$(grep -oP '(?<=    )[a-zA-Z]+(?= = with pkgs;)' "$CONFIG_FILE")"
+
+    for category in $categories; do
+        echo "\e[32mDo you want to install packages in the '$category' category? (y/N)\e[97m"
+        read -r answer
+        if [ "$answer" = "y" ]; then
+            nix-env -iA "nixpkgs.$category"
+        fi
+    done
+}
+
+nix_setup_all() {
+    CONFIG_FILE="$HOME/.config/nixpkgs/config.nix"
+    categories="$(grep -oP '(?<=    )[a-zA-Z]+(?= = with pkgs;)' "$CONFIG_FILE")"
+
+    for category in $categories; do
+		nix-env -iA "nixpkgs.$category"
+    done
+}
+
 # Detect local user installation
 if [ -f ${HOME}/.nix-profile/etc/profile.d/nix.sh ]
 then
 	source ${HOME}/.nix-profile/etc/profile.d/nix.sh
 fi
+
+
