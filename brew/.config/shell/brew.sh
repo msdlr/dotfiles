2e2bac94c4006b4e352f47326b142eac393aaf8b
msdlr <miguelsrosa97@gmail.com>
2025-03-26 17:25:55 +0100

    brew: fix PATH forming

--- brew/.config/shell/brew.sh
+++ brew/.config/shell/brew.sh
@@ -8,6 +8,6 @@ then
 	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
-[ -d ${homebrew_prefix}/bin ] && [[ ":${PATH}:" == *"${homebrew_prefix}/bin"* ]] || export PATH=${homebrew_prefix}/bin:${PATH}
+[ -d "${homebrew_prefix}/bin" ] && echo "$PATH" | grep -q "${homebrew_prefix}/bin" || export PATH="${homebrew_prefix}/bin:${PATH}"
 # [ -d ${homebrew_prefix}/lib ] && [[ ":${LD_LIBRARY_PATH}:" == *"${homebrew_prefix}/lib"* ]] || export LD_LIBRARY_PATH=${homebrew_prefix}/lib:${LD_LIBRARY_PATH}
 
