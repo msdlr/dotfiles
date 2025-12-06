67a8b09ca4fd62b87d3a186d7ffb2f0589fd03df
msdlr <miguelsrosa97@gmail.com>
2025-12-06 23:29:00 +0100

    Remove separate scrits repo from PATH

--- shell/.config/shell/path.sh
+++ shell/.config/shell/path.sh
@@ -4,8 +4,6 @@
 [ -d ${HOME}/.local/bin ] && [[ ":${PATH}:" == *"${HOME}/.local/bin"* ]] || export PATH=${HOME}/.local/bin:${PATH}
 [ -d ${HOME}/.local/lib ] && [[ ":${LD_LIBRARY_PATH}:" == *"${HOME}/.local/lib"* ]] || export LD_LIBRARY_PATH=${HOME}/.local/lib:${LD_LIBRARY_PATH}
 
-# PATH for scripts directory
-[ -d ${HOME}/src/sh ] && [[ ":${PATH}:" == *"${HOME}/src/sh"* ]] || export PATH=${HOME}/src/sh:${PATH}
 
 stty -ixon # disable CTRL-S/Q
 
