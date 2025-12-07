b58e5aa0ae3827b80f2f258a3ad80fae8cc8e269
msdlr <miguelsrosa97@gmail.com>
2025-12-07 17:01:25 +0100

    Change $PATH config to env.sh configuration

--- shell/.config/shell/env.sh
+++ shell/.config/shell/env.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env sh
 
+# PATH + LD for .local/{bin,lib,etc...} for local, non-root user
+[ -d ${HOME}/.local/bin ] && [[ ":${PATH}:" == *"${HOME}/.local/bin"* ]] || export PATH=${HOME}/.local/bin:${PATH}
+[ -d ${HOME}/.local/lib ] && [[ ":${LD_LIBRARY_PATH}:" == *"${HOME}/.local/lib"* ]] || export LD_LIBRARY_PATH=${HOME}/.local/lib:${LD_LIBRARY_PATH}
+
+stty -ixon # disable CTRL-S/Q
+
 # Load XDG directories
 [ -f ${HOME}/.config/user-dirs.dirs ] && . ${HOME}/.config/user-dirs.dirs
 
