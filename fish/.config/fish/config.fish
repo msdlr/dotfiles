793b8a352e79dec72efc57f5155dbc1edf52f1bd
msdlr <miguelsrosa97@gmail.com>
2025-12-09 17:58:47 +0100

    Add initial fish config

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,13 @@
+# --- Movement ---
+# Ctrl+← and Ctrl+→ move by word
+bind \e\[1\;5D backward-word
+bind \e\[1\;5C forward-word
+
+# Ctrl+A / Ctrl+E move to beginning/end of line
+bind \ca beginning-of-line
+bind \ce end-of-line
+
+# Home / End keys
+bind \e\[H beginning-of-line
+bind \e\[F end-of-line
+
