b19e8c484058977da2899e1f868cc61b99f24409
msdlr <miguelsrosa97@gmail.com>
2026-01-19 17:47:15 +0100

    brew: change sourceable script to init at install

--- brew/.config/shell/brew.sh
+++ brew/.config/shell/brew.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env sh
 
-if [ -d "/opt/homebrew" ]; then
+if [ "$(uname)" = "Darwin" ]; then
     HOMEBREW_PREFIX="/opt/homebrew"  # MacOS 
-elif [ -d "/home/linuxbrew/.linuxbrew" ]; then
+elif [ "$(uname)" = "Linux" ]; then
     HOMEBREW_PREFIX="/home/linuxbrew/.linuxbrew"  # Linux
 else
+    echo "Unsupported OS"
+    return 1
+fi
+
+if [ ! -f "${HOMEBREW_PREFIX}/bin/brew" ]; then
 	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
