cd98cdd3a80eec35aa43b3b379ff553d6b9529da
msdlr <miguelsrosa97@gmail.com>
2026-02-03 20:08:06 +0100

    python user venv: improve venv existence check and source using python3

--- python-user-venv/.config/shell/python-user-venv.sh
+++ python-user-venv/.config/shell/python-user-venv.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env sh
 
 PYTHON_HOME_VENV="${HOME}/.local/opt/python"
-mkdir -pv ${PYTHON_HOME_VENV}
 
-if [ ! -d "${PYTHON_HOME_VENV}" ]
+if [ ! -d "${PYTHON_HOME_VENV}" ] || [ ! -f "${PYTHON_HOME_VENV}/bin/activate" ]
 then
     echo "Creating Python venv..."
-    python -m venv "${PYTHON_HOME_VENV}"
+    mkdir -pv ${PYTHON_HOME_VENV}
+    python3 -m venv "${PYTHON_HOME_VENV}"
 fi
 
 source ${PYTHON_HOME_VENV}/bin/activate
